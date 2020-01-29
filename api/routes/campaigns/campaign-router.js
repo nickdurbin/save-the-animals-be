c1f7fdb9f7f5fe4647db99fe0cae1e6609c98745
ndurbin <nick.durbin@gmail.com>
2020-01-29 12:22:30 -0500

    Initial basic routes created for campaigns

---
+++ api/routes/campaigns/campaign-router.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const Campaigns = require("./campaign-model")
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+  try {
+    const campaigns = await Campaigns.find()
+   
+    res.json(campaigns)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const campaign = await Campaigns.findById(id)
+    return res.json(campaign)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post("/", async (req, res, next) => {
+  try {
+    const [id] = await db("campaigns").insert(req.body)
+    const newCampaign= await db("campaigns").where('id', id).first()
+    return res.status(201).json(newCampaign)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const campaign = req.body
+    
+    await Campaigns.update(id, campaign)
+    
+    const newCampaign = await Campaigns.findById(id)
+
+    return res.status(200).json(newCampaign)
+  } catch(error) {
+    next(error)
+  }
+})
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Campaigns.remove(req.params.id)
+    return res.status(200).json({ message: "Campaign successfully deleted!" })
+  } catch(error) {
+    next(error)
+  }
+})
+
+module.exports = router;
