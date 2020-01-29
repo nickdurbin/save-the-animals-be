4aa55d303043171ae3ea2577ffe87214bf1905af
ndurbin <nick.durbin@gmail.com>
2020-01-29 16:08:16 -0500

    Added a testing and production configuration w/ pg

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,39 @@ module.exports = {
     },
   },
 
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+
 };
