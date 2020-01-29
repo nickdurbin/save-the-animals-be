3a4861d999f2b7ca7e0af941908027edb10c31cb
ndurbin <nick.durbin@gmail.com>
2020-01-29 16:09:44 -0500

    Added the campaign router.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const authRouter = require('./auth/auth-router')
+const campaignRouter = require('./campaigns/campaign-router')
 const userRouter = require('./users/user-router')
 
 module.exports = server => {
   server.use("/api/auth", authRouter)
+  server.use("api/campaigns", campaignRouter)
   server.use("/api/users", userRouter)
 }
