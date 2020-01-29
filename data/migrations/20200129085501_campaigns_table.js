6e61b5090d98d7180cebdccd8cce72c8fbd59001
ndurbin <nick.durbin@gmail.com>
2020-01-29 09:46:47 -0500

    Initials campaigns schema created

---
+++ data/migrations/20200129085501_campaigns_table.js
@@ -0,0 +1,36 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("campaigns", (table) => {
+    table.increments()
+    
+    table.string("title")
+      .unique()
+      .notNullable()
+    table.string("urgency", 128)
+      .notNullable()
+    table.string("location")
+      .notNullable()
+    table.date("date")
+      .notNullable()
+    table.string("description", 255)
+      .notNullable()
+    table.integer("animal_id")
+      .unsigned()
+    table.foreign("animal_id")
+      .notNullable()
+      .references("id")
+      .inTable("animals")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+    table.float("funding_goal")
+      .notNullable()
+    table.float("donations")
+    table.text("images")
+    table.boolean("completed")
+      .notNullable()
+      .defaultTo(false)
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("campaigns")
+};
