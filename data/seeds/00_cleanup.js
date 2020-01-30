7d6231fd83e274ece7ce39da3569a24ca1929bb6
nick.durbin <nick.durbin@gmail.com>
2020-01-29 20:48:40 -0500

    Changed the cleanup to include tbale changes

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,7 +1,7 @@
 exports.seed = async (knex) => {
+  await knex("campaign_supporters").truncate()
   await knex("supporters").truncate()
   await knex("organizations").truncate()
   await knex("campaigns").truncate()
-  await knex("animals").truncate()
   await knex("users").truncate()
 }
