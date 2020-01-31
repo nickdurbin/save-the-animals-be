1a76eee53442ecde0ac9bf7d1d5f1c119de74d48
ndurbin <nick.durbin@gmail.com>
2020-01-31 10:32:55 -0500

    Added the organizations router.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const authRouter = require('./auth/auth-router')
 const campaignRouter = require('./campaigns/campaign-router')
 const userRouter = require('./users/user-router')
+const organizationRouter = require('./organizations/organization-router')
 
 module.exports = server => {
   server.use("/api/auth", authRouter)
-  server.use("api/campaigns", campaignRouter)
+  server.use("/api/campaigns", campaignRouter)
+  server.use("/api/organizations", organizationRouter)
   server.use("/api/users", userRouter)
 }
