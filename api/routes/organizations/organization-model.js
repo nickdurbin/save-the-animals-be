c751d0160bd27df60deeafb9312570da1c10e0c5
nick.durbin <nick.durbin@gmail.com>
2020-01-30 23:59:47 -0500

    Added an org model.

---
+++ api/routes/organizations/organization-model.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../../data/db-config')
+
+function find() {
+  return db("organizations").select("id", "email", "username", "role")
+}
+
+function findBy(filter) {
+  return db("organizations").where(filter).select("id", "email", "username", "org_name")
+}
+
+function add(organization) {
+  organization.password = bcrypt.hashSync(organization.password, 14)
+  db("organizations").insert(organization).returning("*")
+}
+
+function findById(id) {
+  return db("organizations").where({ id }).first("id", "username", "org_name", "org_description")
+}
+
+function update(id, changes) {
+  return db("organizations")
+    .where({ id })
+    .update(changes)
+    .returning("*")
+}
+
+function remove(id) {
+  return db("organizations")
+    .where({ id })
+    .del()
+}
+
+module.exports = {
+  add, 
+  find, 
+  findBy, 
+  findById,
+  remove,
+  update
+}
