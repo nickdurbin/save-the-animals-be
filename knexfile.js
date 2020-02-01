a42dae861561b2fe7111b30ab8fd92471cb70319
ndurbin <nick.durbin@gmail.com>
2020-02-01 18:58:37 -0500

    Removed an unnecessary comment.

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ const localPg = {
 
 const pg = require('pg');
 pg.defaults.ssl = true;
-const dbConnection = process.env.DATABASE_URL || localPg; // used to prevent random errors
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
 
