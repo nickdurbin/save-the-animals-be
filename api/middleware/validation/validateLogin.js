653e021cac16fca62ed36ed0efddb48df4ee9736
ndurbin <nick.durbin@gmail.com>
2020-02-01 11:03:52 -0500

    Changed from username to email.

--- api/middleware/validation/validateLogin.js
+++ api/middleware/validation/validateLogin.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
   if (!req.body.email || !req.body.password) {
     res
       .status(400)
-      .json({ error: "Both a username and password are required for login" });
+      .json({ error: "Both an email and password are required for login" });
   } else {
     next();
   }
