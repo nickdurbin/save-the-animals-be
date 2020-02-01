699f292ce5325c8ab7093ddbbf6dcc224cb1a1c8
ndurbin <nick.durbin@gmail.com>
2020-02-01 18:59:19 -0500

    Added a support router.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,13 @@
 const authRouter = require('./auth/auth-router')
 const campaignRouter = require('./campaigns/campaign-router')
-const userRouter = require('./users/user-router')
 const organizationRouter = require('./organizations/organization-router')
+const supporterRouter = require('./supporters/supporter-router')
+const userRouter = require('./users/user-router')
 
 module.exports = server => {
   server.use("/api/auth", authRouter)
   server.use("/api/campaigns", campaignRouter)
   server.use("/api/organizations", organizationRouter)
+  server.use("/api/supporters", supporterRouter)
   server.use("/api/users", userRouter)
 }
