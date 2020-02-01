d81787543f0fea4bca01b724e73ab3e9176fb025
ndurbin <nick.durbin@gmail.com>
2020-02-01 18:59:32 -0500

    Injected sendgrid to send emails upon reg

--- api/routes/auth/auth-router.js
+++ api/routes/auth/auth-router.js
@@ -8,12 +8,24 @@ const Users = require('../users/user-model')
 const Organizations = require('../organizations/organization-model')
 const router = express.Router()
 const Auth = require('../auth/auth_model')
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post("/register/supporter", validateSupReg, async (req, res, next) => {
   try {
-    const user = await Users.add(req.body)
+    const user = await Users.add(req.body) 
+    const msg = {
+      to: 'savetheanimalsbw@gmail.com',
+      from: 'savetheanimalsbw@gmail.com',
+      subject: 'Thank you for Registering an Account at Save the Animals',
+      text: 'Thank you.',
+      html: `Welcome ${user.first_name}, <br /> We are glad you chose to create an account with us!<br /><strong>Sincerly, Save the Animals!</strong>`,
+    };
     
     if (user) {
+      console.log(user)
+      sgMail.send(msg);   
       return res.status(201).json({ message: "User has been successfully registered.", user})
     }
 
@@ -25,8 +37,17 @@ router.post("/register/supporter", validateSupReg, async (req, res, next) => {
 router.post("/register/organization", validateOrgReg, async (req, res, next) => {
   try {
     const organization = await Organizations.add(req.body)
+    const msg = {
+      to: 'savetheanimalsbw@gmail.com',
+      from: 'savetheanimalsbw@gmail.com',
+      subject: 'Thank you for Registering an Account at Save the Animals',
+      text: 'Thank you.',
+      html: `Welcome ${organization.org_name}, <br /> We are glad you chose to create an account with us!<br /><strong>Sincerly, Save the Animals!</strong>`,
+    };
     
     if (organization) {
+      console.log(organization)
+      sgMail.send(msg);
       return res.status(201).json({ message: "Organization has been successfully registered.", organization })
     } 
   } catch (error) {
