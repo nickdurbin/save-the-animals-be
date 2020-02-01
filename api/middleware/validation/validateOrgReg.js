ef855ac6ec2f10b40d2cefd921284c146b64d01f
ndurbin <nick.durbin@gmail.com>
2020-02-01 11:04:10 -0500

    Changed info expected in error message

--- api/middleware/validation/validateOrgReg.js
+++ api/middleware/validation/validateOrgReg.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   if (!req.body.email || !req.body.username || !req.body.password || !req.body.org_name) {
     res.status(400).json({ 
-      error:"Make sure to enter the following data:  email, username, password, first_name, and last_name."
+      error:"Make sure to enter the following data: email, username, password, and org_name."
       });
   } else {
     next();
