848558ae51e13f920923b0f2b638e5dedd082274
ndurbin <nick.durbin@gmail.com>
2020-02-01 09:11:24 -0500

    Middleware to handle Supporter Registration

---
+++ api/middleware/validation/validateSupReg.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  if (!req.body.email || !req.body.username || !req.body.password || !req.body.first_name || !req.body.last_name) {
+    res.status(400).json({ 
+      error:"Make sure to enter the following data:  email, username, password, first_name, and last_name."
+      });
+  } else {
+    next();
+  }
+};
