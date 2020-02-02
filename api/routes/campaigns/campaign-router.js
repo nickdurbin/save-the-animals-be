57033c2dff681a65cc2b7a66eff42dc2b98a3ac2
nick.durbin <nick.durbin@gmail.com>
2020-02-02 17:02:12 -0500

    Adjusted a few add functions.

--- api/routes/campaigns/campaign-router.js
+++ api/routes/campaigns/campaign-router.js
@@ -29,8 +29,8 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const [id] = await db("campaigns").insert(req.body)
-    const newCampaign= await db("campaigns").where('id', id).first()
+    const [id] = await Campaigns.add(req.body)
+    const newCampaign= await Campaigns.findById("id", id)
     return res.status(201).json(newCampaign)
   } catch (err) {
     next(err)
