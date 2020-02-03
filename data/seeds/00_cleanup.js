3608a17fd59e4d185deb7fee55316241893b7e33
ndurbin <nick.durbin@gmail.com>
2020-02-03 12:44:53 -0500

    Changed the order of cleanup.

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,6 +1,6 @@
 exports.seed = async (knex) => {
   await knex("supporters").truncate()
-  await knex("organizations").truncate()
   await knex("campaigns").truncate()
+  await knex("organizations").truncate()
   await knex("users").truncate()
 }
