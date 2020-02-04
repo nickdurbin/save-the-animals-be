105529040a03d3c8dca7e2ffd086cd77b94562af
ndurbin <nick.durbin@gmail.com>
2020-02-04 09:36:43 -0500

    Injected the xss middleware.

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,9 +1,11 @@
 const helmet = require('helmet')
+const xss = require('xss-cleans')
 const morgan = require('morgan')
 const cors = require('cors')
 
 module.exports = server => {
   server.use(morgan("short"))
   server.use(helmet())
+  server.use(xss())
   server.use(cors())
 }
